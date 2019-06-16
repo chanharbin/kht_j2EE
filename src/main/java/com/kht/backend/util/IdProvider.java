31a5af37206ba91d43d3e6fee9179df8ef878cd6
madfff <896316348@qq.com>
2019-06-16 15:37:43 +0800

    create idProvider

---
+++ src/main/java/com/kht/backend/util/IdProvider.java
@@ -0,0 +1,99 @@
+package com.kht.backend.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IdProvider {
+
+    private long twepoch=1560667623144L;
+
+    private final long sequenceBits=2L;
+
+    private final long timestampLeftShift=sequenceBits;
+
+    private final long sequenceMask=-1L^(-1L<<sequenceBits);
+
+    private long sequence=0L;
+
+    private long lastTimestamp=-1L;
+
+    private final long charBits=5;
+
+    private final long charMasks=-1L^(-1L<<charBits);
+
+    private final char[] bitTochar={'0','1','2','3','4', '5','6','7','8','9', 'A','B','C','D','E', 'F','G','H','J','K', 'L','M','N','P','Q', 'R','S','T','U','V', 'W','S'};
+
+    public IdProvider(){}
+
+    /**
+     *
+     * @param prefix
+     * @return 生成不重复的id
+     */
+    public String getId(String prefix)
+    {
+        long suffix=nextId();
+        char[] tempString=new char[8];
+        for(int i=7;i>=0;i--)
+        {
+            tempString[i]=bitTochar[(int)(suffix&charMasks)];
+            suffix>>=charBits;
+        }
+        return prefix+new String(tempString);
+    }
+    /**
+     * 线程安全
+     * @return 返回不重复的id
+     */
+
+    protected synchronized long nextId()
+    {
+        long timestamp=timeGen();
+        if(timestamp<lastTimestamp)
+        {
+            throw new RuntimeException(
+                    String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
+        }
+        if(timestamp==lastTimestamp)
+        {
+            sequence=(sequence+1)&sequenceMask;
+            if(sequence==0)
+            {
+                timestamp=tilNextMillis(timestamp);
+            }
+            else
+            {
+                sequence=0L;
+            }
+            //System.out.println("fuck_timestamp: "+timestamp);
+        }
+        lastTimestamp=timestamp;
+        //System.out.println("fuck_timestamp: "+timestamp);
+        //System.out.println("fuck_sequence: "+sequence);
+        return ((timestamp-twepoch))<<timestampLeftShift|sequence;
+    }
+
+    /**
+     * 延迟1ms
+     * @param lastTimestamp
+     * @return 延迟后的时间戳
+     */
+    protected long tilNextMillis(long lastTimestamp)
+    {
+        System.out.println("lastTimestamp: "+lastTimestamp);
+        long timestamp=timeGen();
+        while(timestamp<=lastTimestamp)
+        {
+            timestamp=timeGen();
+        }
+        return  timestamp;
+    }
+    /**
+     *
+     * @return 当前时间戳
+     */
+    protected long timeGen()
+    {
+        return System.currentTimeMillis();
+    }
+}
---
+++ src/test/java/com/kht/backend/util/IdProviderTest.java
@@ -0,0 +1,38 @@
+package com.kht.backend.util;
+
+import com.kht.backend.App;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import  com.kht.backend.util.IdProvider;
+
+import static org.junit.Assert.*;
+@SpringBootTest(classes = App.class)
+// 让 JUnit 运行 Spring 的测试环境， 获得 Spring 环境的上下文的支持
+@RunWith(SpringRunner.class)
+public class IdProviderTest {
+    @Autowired
+    IdProvider idProvider;
+    @Test
+    public void getId() {
+        for(int i=0;i<5;i++) {
+            System.out.println(idProvider.getId("fuck"));
+        }
+    }
+
+    @Test
+    public void nextId() {
+        for(int i=0;i<10;i++) {
+            System.out.println(idProvider.nextId());
+        }
+    }
+
+    @Test
+    public void timeGen() {
+        for(int i=0;i<10;i++) {
+            System.out.println(idProvider.timeGen());
+        }
+    }
+}
