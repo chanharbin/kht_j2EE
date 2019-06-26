b3fe1b36ac4feb6826871b18cea60609b88314d6
TheEnd_py <1612777928@qq.com>
2019-06-26 16:19:54 +0800

    Modified.

--- src/main/java/com/kht/backend/service/impl/OperationLogServiceImpl.java
+++ src/main/java/com/kht/backend/service/impl/OperationLogServiceImpl.java
@@ -3,14 +3,17 @@ package com.kht.backend.service.impl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.kht.backend.dao.OperaLogDOMapper;
+import com.kht.backend.dataobject.OperaLogDO;
 import com.kht.backend.entity.Result;
 import com.kht.backend.service.OperationLogService;
 import com.kht.backend.service.model.OperationLogModel;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
 import java.util.List;
-/*
+import java.util.Map;
+
 @Service
 public class OperationLogServiceImpl implements OperationLogService {
 
@@ -21,15 +24,40 @@ public class OperationLogServiceImpl implements OperationLogService {
     private int pageSize;
 
     @Override
-    public Result getOperationLogsByTime(int pageNum, Long startTime, Long endTime) {
+    public Result getAllOperationLogs(int pageNum) {
         PageHelper.startPage(pageNum,pageSize);
         List<OperationLogModel> operationLogModelList = operaLogDOMapper.listAll();
-        PageInfo<DataDictionaryModel> page = new PageInfo<>(dataDictionaryModelList);
+        PageInfo<OperationLogModel> page = new PageInfo<>(operationLogModelList);
         Map<String, Object> resultData = new LinkedHashMap<>();
         resultData.put("totalNum",page.getTotal());
         resultData.put("data",page.getList());
         return Result.OK(resultData).build();
     }
-}
 
- */
+    @Override
+    public Result getOperationLogsByEmployeeCode(int pageNum, String employeeCode) {
+        PageHelper.startPage(pageNum,pageSize);
+        List<OperationLogModel> operationLogModelList = operaLogDOMapper.selectByEmployeeCode(employeeCode);
+        PageInfo<OperationLogModel> page = new PageInfo<>(operationLogModelList);
+        Map<String, Object> resultData = new LinkedHashMap<>();
+        resultData.put("totalNum",page.getTotal());
+        resultData.put("data",page.getList());
+        return Result.OK(resultData).build();
+    }
+
+    @Override
+    public Result getOperationLogsByLogTime(int pageNum, Long startTime, Long endTime) {
+        PageHelper.startPage(pageNum,pageSize);
+        List<OperationLogModel> operationLogModelList = operaLogDOMapper.selectByLogTime(startTime, endTime);
+        PageInfo<OperationLogModel> page = new PageInfo<>(operationLogModelList);
+        Map<String, Object> resultData = new LinkedHashMap<>();
+        resultData.put("totalNum",page.getTotal());
+        resultData.put("data",page.getList());
+        return Result.OK(resultData).build();
+    }
+
+    @Override
+    public boolean addOperationLog(OperaLogDO operaLogDO) {
+        return false;
+    }
+}
