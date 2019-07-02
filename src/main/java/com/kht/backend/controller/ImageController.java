6278fc8b13580bfee6d49c34139eead6dca7e307
TheEnd_py <1612777928@qq.com>
2019-07-02 16:43:53 +0800

    Add a service about saving images.

--- src/main/java/com/kht/backend/controller/ImageController.java
+++ src/main/java/com/kht/backend/controller/ImageController.java
@@ -3,8 +3,12 @@ package com.kht.backend.controller;
 import com.kht.backend.entity.Result;
 import com.kht.backend.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 public class ImageController {
@@ -13,7 +17,8 @@ public class ImageController {
     private ImageService imageService;
 
 
-    public Result saveImage(MultipartFile idFront) {
+    @RequestMapping(value = "/image", method = POST, produces = "application/json;charset=UTF-8")
+    public Result saveImage(@RequestParam("idFront") MultipartFile idFront) {
         return imageService.saveImage(idFront);
     }
 }
